9f1c37407203013987fdd0cbed6b6fee4f27aea2
haozhang <799407209@qq.com>
2024-04-06 17:48:13 +0800

    完成16.1章，下一步第16.2章

--- highs_lows.py
+++ highs_lows.py
@@ -15,18 +15,15 @@ with open(filename) as f:
 
         low = int(row[3])
         lows.append(low)
-
 # 根据数据绘制图形
 fig = plt.figure(dpi=128, figsize=(10, 6))
 plt.plot(dates,highs, c='red',alpha = 0.5)
 plt.plot(dates,lows, c='blue',alpha = 0.5)
 plt.fill_between(dates,highs,lows,facecolor = 'blue',alpha=0.1)
-
 # 设置图形的格式
 plt.title("Daily high temperatures, 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
-
 plt.show()
